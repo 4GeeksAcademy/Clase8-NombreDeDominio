1204d4a5537fe231ede4ccc534689e6edb16f86d
FrancoRegg <138858381+FrancoRegg@users.noreply.github.com>
2023-08-17 09:33:23 +0000

    Domain funcionando en consola

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extensiones = ["com", "es", "ar", "net"];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extensiones.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + "." + extensiones[l]);
+        }
+      }
+    }
+  }
 };
